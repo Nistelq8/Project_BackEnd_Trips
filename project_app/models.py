ac06b94bae0810f6652d17b1348e4e688b26c5fc
Nistelq8 <aziz.almarzouq@gmail.com>
2022-08-05 01:29:22 +0300

    changed trip to use user instead of userprofile

--- project_app/models.py
+++ project_app/models.py
@@ -22,6 +22,6 @@ class Trip(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     pic = models.ImageField(upload_to='media/')
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="trips")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="trips")
     id = models.AutoField(primary_key=True)
 
